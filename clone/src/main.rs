d3060a58d01aed0f9c8bb39eb7416edddfcf6d64
ah953 <palindrome0223@gmail.com>
2022-07-02 15:24:19 -0400

    add: calculate_length function

--- clone/src/main.rs
+++ clone/src/main.rs
@@ -1,15 +1,23 @@
 fn main() {
+    let s = gives_ownership();
+
+    let s1 = String::from("hi");
+    let s2 = takes_and_gives_back(s1);
+    println!("{}, {}", s, s2);
+    let (s3, len) = calculate_length(s);
+    println!("{}, {}", s3, len);
+}
+
+fn gives_ownership() -> String{
     let s = String::from("hello");
-    take_ownership(s);
-
-    let x = 5;
-    makes_copy(x);
+    s
 }
 
-fn take_ownership(some_string: String){
-    println!("{}", some_string);
+fn takes_and_gives_back(s: String) -> String{
+    s
 }
 
-fn makes_copy(some_integer: i32) {
-    println!("{}", some_integer);
+fn calculate_length(s: String) -> (String, usize) {
+    let length = s.len();
+    (s, length)
 }
